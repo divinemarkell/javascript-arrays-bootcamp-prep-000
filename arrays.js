e74afe4412545d7e2c7bfcc3f3e848fb9fa661bc
Markell Owens <markellowens@mac.com>
2018-07-10 02:53:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,29 +8,28 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   return array;
 }
 function addElementToEndOfArray (array, element) {
-  array.push(element)
-  return array
+  return [...array, element];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
 function accessElementInArray (array, index) {
-  return array[index]
+  return array[index];
 }
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-array.shift()
-return array
+array.shift();
+return array;
 }
 function removeElementFromBeginningOfArray (array) {
-  return array.slice(1)
+  return array.slice(1);
   
 }
 function destructivelyRemoveElementFromEndOfArray (array) {
-  array.pop ()
-  return array
+  array.pop ();
+  return array;
 }
 function removeElementFromEndOfArray (array) {
-  return array.slice(0, array.length -1)
+  return array.slice(0, array.length -1);
 
 }
